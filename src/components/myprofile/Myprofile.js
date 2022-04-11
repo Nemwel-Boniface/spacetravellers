cf731df4607d2ccdc5877bdffba18c614a028260
AckonSamuel <ackons433@gmail.com>
2022-04-11 11:08:32 +0100

    Export my profile component

--- src/components/myprofile/Myprofile.js
+++ src/components/myprofile/Myprofile.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Myprofile = () => (
+  <>
     <section className="my-missions">
-        <h2>My Mission</h2>
+      <h2>My Mission</h2>
     </section>
-    
-)
+    <section className="my-missions">
+      <h2>My Mission</h2>
+    </section>
+  </>
+);
+
+export default Myprofile;
