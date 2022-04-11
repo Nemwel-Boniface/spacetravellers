c33f1fec5eaf10cc327c65506580a3061ceb8767
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-11 16:24:06 +0300

    Styling for the myProfile component

--- src/components/populateprofile/Populateprofile.js
+++ src/components/populateprofile/Populateprofile.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './populateprofile.css';
 
 const Populateprofile = () => (
-  <>
+  <div className='populate'>
     <span>Hello Profile!</span>
-  </>
+    <hr/>
+  </div>
 );
 
 export default Populateprofile;
