80cde608767d745322827e0b4faa45970dbd93ff
AckonSamuel <ackons433@gmail.com>
2022-04-11 10:03:51 +0100

    Export navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
-const NavBar = () => {
-    <nav>
-        <span className="rocket"><Link to="/">Rockets</Link></span>
-        <span className="missions"><Link to="/">Missions</Link></span>
-        <span className="profile"><Link to="/">My Profile</Link></span>
-    </nav>
-};
+const NavBar = () => (
+  <nav>
+    <span className="rocket"><Link to="/">Rockets</Link></span>
+    <span className="missions"><Link to="/missions">Missions</Link></span>
+    <span className="profile"><Link to="/myprofile">My Profile</Link></span>
+  </nav>
+);
+
+export default NavBar;
