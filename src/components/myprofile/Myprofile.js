fc6b3a7253f87a61c8594768777aa8e38a1f4d94
AckonSamuel <ackons433@gmail.com>
2022-04-11 11:20:19 +0100

    Import Populateprofile component in Myprofile component

--- src/components/myprofile/Myprofile.js
+++ src/components/myprofile/Myprofile.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Populateprofile from '../populateprofile/Populateprofile';
 
 const Myprofile = () => (
   <>
     <section className="my-missions">
       <h2>My Mission</h2>
+      <Populateprofile />
     </section>
     <section className="my-missions">
       <h2>My Mission</h2>
+      <Populateprofile />
     </section>
   </>
 );
