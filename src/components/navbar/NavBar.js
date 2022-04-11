411036ce456ffe7542c7beb093cbec713c5b62c6
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-11 15:41:44 +0300

    Fix linters

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './navbar.css';
 
 const NavBar = () => (
-  <nav className='navbar'>
+  <nav className="navbar">
     <span className="rocket">Rockets</span>
     <span className="missions">Missions</span>
     <span className="profile">My Profile</span>
