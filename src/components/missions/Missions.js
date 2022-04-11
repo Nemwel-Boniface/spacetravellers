963c9a5ab782f74c15775027c7879e169a28cf71
AckonSamuel <ackons433@gmail.com>
2022-04-11 23:54:47 +0100

    Populate mission details with mission data

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import './missions.css';
+import MissionDetails from './MissionDetails';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missionReducer.missions, shallowEqual);
   return (
     missions.map((mission) => (
-))
+      <MissionDetails
+        name={mission.mission_name}
+        dex={mission.mission_description}
+        key={mission.mission_id}
+      />
+    ))
   );
 };
 
