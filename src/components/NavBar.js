3aa64dfac7c8aaa2182d18cb135d6f64442ed8d8
AckonSamuel <ackons433@gmail.com>
2022-04-11 09:47:05 +0100

    Add logo image in NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+
+const NavBar = () => {
+  <nav>
+    <span id="logo">
+      <img src="../../planet.png" alt="logo" />
+    </span>
+  </nav>;
+};
