cfbecc6614e26d86672a4f03ca42aeedb8e527c9
AckonSamuel <ackons433@gmail.com>
2022-04-11 12:39:24 +0100

    Create basic structure for mission setup

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,1 +1,9 @@
 import React from 'react';
+
+const Missions = () => (
+  <div className="mission-page">
+    <div className="mission-title"></div>
+    <div className="mission-description"></div>
+    <div className="mission-status"></div>
+  </div>
+);
