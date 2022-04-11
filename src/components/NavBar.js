6b8d3359cb80a28e23aac0987795d7886113879c
AckonSamuel <ackons433@gmail.com>
2022-04-11 09:48:44 +0100

    Add file for header component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,8 @@ import { Link } from 'react-router-dom';
 import './navbar.css';
 
 const NavBar = () => {
-  <nav>
-    <span id="logo">
-      <img src="../../planet.png" alt="logo" />
-    </span>
-  </nav>;
+  <span id="logo">
+    <img src="../../planet.png" alt="logo" />
+    <span>Space Travelers' Hub</span>
+  </span>;
 };
