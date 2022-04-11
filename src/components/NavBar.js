ca097cf4e4110bc92529159ed03936aa20e6227c
AckonSamuel <ackons433@gmail.com>
2022-04-11 09:56:19 +0100

    Remove logo elements from navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,5 @@ import { Link } from 'react-router-dom';
 import './navbar.css';
 
 const NavBar = () => {
-  <span id="logo">
-    <img src="../../planet.png" alt="logo" />
-    <span>Space Travelers' Hub</span>
-  </span>;
+
 };
