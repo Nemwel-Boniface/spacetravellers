d86281b82e2165c0f75a9f4c899a38e97e527c1c
AckonSamuel <ackons433@gmail.com>
2022-04-11 09:55:36 +0100

    Add logo image in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Header = () => {
-    <header>
-        <div className="logo">
-            <span>Space Travelers' Hub</span>
-        </div>
-    </header>
-}
+  <header>
+    <div className="logo">
+      <img src="../../planets.png" alt="logo" />
+      <span>Space Travelers' Hub</span>
+    </div>
+  </header>;
+};
