6582746e3089ed0891f20e3fd3c821ecaeebc109
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-12 06:04:08 +0300

    Update included middleware to the store

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import missionReducer from './missions/missions';
 import RocketsRuducer from './Rockets/rockets';
 
@@ -7,6 +8,9 @@ const reducers = combineReducers({
   RocketsRuducer,
 });
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+  );
 
 export default store;
