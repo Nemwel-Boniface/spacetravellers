6d063a7a09df6595cf354495d4000ab212a581e0
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-12 06:05:59 +0300

    Update created the individual rocket's card component

---
+++ src/components/Rockets/Rocket.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Rocket = (props) => {
+  const { rocket } = props;
+  const { name, type, flickr_images } = rocket;
+  return (
+    <div className="rocket">
+
+    </div>
+  )
+}
+export default Rocket;
