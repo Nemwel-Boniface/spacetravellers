ca230eb6bc10d04a87fadbf6a174305d55fd5832
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-12 06:05:12 +0300

    Update connected the Rockets component with the Redux store

--- src/components/Rockets/rockets.js
+++ src/components/Rockets/rockets.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Rocket from './Rocket';
+import { getRocketFromAPI } from '../../redux/Rockets/rockets';
 
-const Rockets = () => (
-  <div className="rockets">
-    <h1>Rockets!</h1>
-  </div>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.RocketsRuducer);
+  useEffect(() => {
+    dispatch(getRocketFromAPI())
+  }, []);
+
+  return (
+    <div className="rockets">
+      <ul>
+        {rockets.map((rocket) => (
+          <Rocket key={rocket.id} rocket={rocket} />
+        ))}
+      </ul>
+    </div>
+  )
+};
 
 export default Rockets;
