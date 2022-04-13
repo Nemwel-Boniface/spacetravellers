acdd9876f19ed7e08aada84ebdf499c15725fc63
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-13 12:56:08 +0300

    Update included markup for displaying the resrved rockets

--- src/components/myprofile/Myprofile.js
+++ src/components/myprofile/Myprofile.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Populateprofile from '../populateprofile/Populateprofile';
 import './myprofile.css';
+import { addRocketToMyProfile } from '../../redux/Rockets/rockets';
 
-const Myprofile = () => (
-  <div className="myProfile">
+const Myprofile = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.RocketsReducer);
+
+  useEffect(() =>{
+    dispatch(addRocketToMyProfile())
+  }, []);
+  return (
+    <div className="myProfile">
     <section className="my-missions">
       <h2>My Mission</h2>
-      <Populateprofile />
+      <table className='reservedCont'>
+        <tbody>
+        {rockets.map((rocket) => (
+         <tr className='reservedItem' key={rocket.id}>{rocket.name}</tr>
+        ))}
+        </tbody>
+      </table>
     </section>
     <section className="my-rockets">
       <h2>My Rockets</h2>
       <Populateprofile />
     </section>
   </div>
-);
+  )
+}
 
 export default Myprofile;
