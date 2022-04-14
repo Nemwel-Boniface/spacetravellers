24bb1cbc1dfff9780be1cdc7a8013a01d3c191a5
AckonSamuel <ackons433@gmail.com>
2022-04-15 00:09:28 +0100

    Add skeleton for mission structure

--- src/components/missions/MissionSkeleton.js
+++ src/components/missions/MissionSkeleton.js
@@ -1,2 +1,12 @@
 import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+
+const MissionSkeleton = (name, description, status) => (
+  <div className="mission-table">
+    <h2>{name}</h2>
+    <div className="mission-description">
+      {description}
+    </div>
+    <div className="mission-status">{status}</div>
+    <button type="button" className="mission-button">Join mission</button>
+  </div>
+);
