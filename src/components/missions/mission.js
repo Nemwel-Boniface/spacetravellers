055f059c992099934db07eb06897a07ef065a4d6
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-14 11:52:59 +0300

    Update included:  - Handler funtion that dispatched:    - Action when user joins a mission assigning reserved to true    - Action when a user leaves a mission assigning reserved back to false  - Gave each table row and buuton a unique id

--- src/components/missions/mission.js
+++ src/components/missions/mission.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { missionJoining, missionLeaving } from '../../redux/missions/missions';
 import './missions.css';
 
 const Mission = (props) => {
@@ -6,13 +8,22 @@ const Mission = (props) => {
   const {
     id, name, description
   } = mission;
+  const dispatch = useDispatch();
+
+  const handleMissions = () => {
+    if(mission.reserved) {
+      dispatch(missionLeaving(mission.id))
+    } else {
+      dispatch(missionJoining(mission.id))
+    }
+  }
   return (
     <>
       <tr id={id}>
         <td className='missionName'>{name}</td>
         <td>{description}</td>
         <td><small className='missionBadge'>Not a member</small></td>
-        <td><button type='button'>Join Mission</button></td>
+        <td><button id={id} onClick={handleMissions} type='button'>Join Mission</button></td>
       </tr>
     </>
   );
