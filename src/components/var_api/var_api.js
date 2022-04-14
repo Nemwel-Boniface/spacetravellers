364f4c9e461b1b518023f4627728c518fa72bbf7
AckonSamuel <ackons433@gmail.com>
2022-04-14 23:39:10 +0100

    Extract relevant parameters from missions api response

--- src/components/var_api/var_api.js
+++ src/components/var_api/var_api.js
@@ -1,6 +1,15 @@
 const baseUrl = 'https://api.spacexdata.com/v3/missions';
 
 const fetchMissions = async () => {
-    const result = await fetch(baseUrl).then((res) => res.json())
-    .
-}
+  const result = await fetch(baseUrl).then((res) => res.json())
+    .then((response) => {
+      response.map((mission) => ({
+        id: mission.mission_id,
+        name: mission.mission_name,
+        description: mission.description,
+        reserved: false,
+      }));
+    });
+
+  return result;
+};
