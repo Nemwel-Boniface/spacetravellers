0f51b3a6278d778b8133800a88308910ecf52c65
AckonSamuel <ackons433@gmail.com>
2022-04-15 00:26:08 +0100

    Create missions component function to render list of missions

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
+import MissionSkeleton from './MissionSkeleton';
 
-const Missions = () => (
-  <div className="mission-page">
-    <div className="mission-title" />
-    <div className="mission-description" />
-    <div className="mission-status" />
-  </div>
-);
+const Missions = () => {
+  const missions = useSelector((state) => state.missionReducer, shallowEqual);
+
+  return (
+    <div className="super-mission">
+      {missions.map((mission) => (
+        <MissionSkeleton
+          key={mission.mission_id}
+          name={mission.mission_name}
+          description={mission.mission_description}
+          status={mission.mission_status}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Missions;
