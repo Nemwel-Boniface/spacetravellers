d4466395a23798259cfd7b86d53d72d428a56a83
AckonSamuel <ackons433@gmail.com>
2022-04-15 13:42:26 +0100

    Write tests for mission component

---
+++ src/__tests__/missions.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import store from '../redux/configStore';
+import Missions from '../components/Rockets/rockets';
+
+describe('A tests for my Rockets component', () => {
+  it('Check whether Rockets component renders', () => {
+    const mission = {
+      id: 1,
+      name: 'Thaicom',
+      description: 'Thaicom is the name of a series of  communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+      reserved: false,
+    };
+    const missionTree = renderer.create(
+      <Provider store={store}>
+        <Missions
+          key={mission.id}
+          description={mission.description}
+          name={mission.name}
+          reserved={mission.reserved}
+        />
+      </Provider>,
+    );
+
+    expect(missionTree).toMatchSnapshot();
+  });
+
+  it('We now check whether it renders the rockets correctly', () => {
+    const missionTree = renderer.create(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+
+    expect(missionTree).toMatchSnapshot();
+  });
+});
